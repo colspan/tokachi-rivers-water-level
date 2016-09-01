dbbc3203ded91060ae9a5cc529d5d984dd0cdfc4
MIYOSHI Kunihiko <contact@colspan.net>
2016-09-02 00:31:55 +0900

    updated water level logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,50 @@
 
-var positions_raw = [
-    ["42度34分48秒", "142度56分05秒"],
-    ["42度34分48秒", "142度56分05秒"],
-    ["42度34分48秒", "142度56分05秒"],
-    ["42度36分12秒", "142度59分13秒"],
-    ["42度38分32秒", "143度05分30秒"],
-    ["42度47分52秒", "143度09分24秒"],
-    ["42度47分52秒", "143度09分24秒"],
-    ["42度50分23秒", "143度11分17秒"],
-    ["42度55分33秒", "143度13分44秒"],
-    ["42度41分58秒", "143度03分30秒"],
-    ["42度43分57秒", "143度06分32秒"],
-    ["42度56分48秒", "143度16分11秒"],
-    ["42度54分48秒", "143度16分55秒"],
-    ["42度54分37秒", "143度21分23秒"],
-    ["43度05分40秒", "143度30分59秒"],
-    ["42度55分52秒", "143度26分36秒"],
-    ["42度55分11秒", "143度28分35秒"],
-    ["42度47分57秒", "143度28分18秒"],
-    ["42度46分27秒", "143度35分50秒"],
-    ["42度44分03秒", "143度40分27秒"]
-];
-
-var positions = [];
-
-positions_raw.forEach(function(d){
-    function parseLonLat(x){
-        var reg = new RegExp("[度分秒]");
-        var row = x.split(reg);
-        return +row[0] + (+row[1])/60 + (+row[2])/60/60;
-    }
-    var lat = parseLonLat(d[0]);
-    var lon = parseLonLat(d[1]);
-    positions.push([lon,lat]);
-});
-
-var domains = [[466.96,486.05],
-[11.93,707.31],
-[16.75,647.93],
-[398.42,401.26],
-[342.76,345.53],
-[247.86,249.42],
-[100.28,104.18],
-[74.59,79.38],
-[33.35,36.67],
-[186.87,188.98],
-[142.82,145.69],
-[27.51,30.19],
-[22.7,26.9],
-[14.08,20.24],
-[34.35,36.64],
-[9.95,15.51],
-[12.9,16.54],
-[7.74,13.59],
-[2.07,5.36],
-[0.07,3.2]];
+var domains = {
+"s301081281107010":[344.37, 347.52],
+"s301081281107020":[142.47,144.09],
+"s301081281107040":[60.03,64.79],
+"s301081281107050":[31.67,38.07],
+"s301081281107055":[21.28,28],
+"s301081281107060":[8.86,18.74],
+"s301081281107070":[2.35,12.68],
+"s301081281107090":[-0.28,1.84],
+"s301081281107100":[475.47,477.69],
+"s301081281107110":[481.92,485.31],
+"s301081281107135":[61.42,64.28],
+"s301081281107145":[41.31,44.3],
+"s301081281107150":[204.48,207.15],
+"s301081281107160":[71.25,74.45],
+"s301081281107170":[31.19,34.46],
+"s301081281107180":[495.38,497.65],
+"s301081281107195":[398.09,401.26],
+"s301081281107200":[342.45,345.53],
+"s301081281107210":[247.72,249.42],
+"s301081281107220":[100.28,104.18],
+"s301081281107230":[74.19,79.38],
+"s301081281107240":[32.28,36.67],
+"s301081281107250":[186.87,188.98],
+"s301081281107260":[142.53,145.69],
+"s301081281107280":[27.29,30.19],
+"s301081281107290":[22.33,26.9],
+"s301081281107300":[13.56,20.24],
+"s301081281107320":[33.55,37.34],
+"s301081281107330":[9.19,15.51],
+"s301081281107350":[12.39,17.08],
+"s301081281107360":[7.46,13.59],
+"s301081281107370":[1.37,5.42],
+"s301081281107400":[-0.15,3.2]
+}
 
 function init(){
+    var rangeY = 40;
+    var siteinfos = {};
+    var scalesY = [];
+    var colorScales = [];
+    var positions = [];
+    var text_date;
+    var projection;    
+    var svg = d3.select("#drawarea").attr("width", 800).attr("height", 620);
+
     /* 値変換 */
     function row(d){
         Object.keys(d).forEach(function(k){
@@ -77,6 +65,19 @@ function init(){
     }
     function ready(data){
         var d = data;
+
+        Object.keys(domains).forEach(function(k){
+            scalesY.push( d3.scaleLinear()
+            .domain(domains[k])
+            .range([0, rangeY]));
+
+            colorScales.push( d3.scaleLinear()
+            .domain(domains[k])
+            .range(["blue","red"]));
+
+            positions.push( siteinfos[k].coordinate );
+        });
+
         var target_index = 0;
         var target_row = get_row(data,target_index);
         update(target_row);
@@ -126,31 +127,15 @@ function init(){
             return y;
         })
         .attr("r", function(d,i) {
-            return scalesY[i](d);
+            var v = scalesY[i](d);
+            if(v<0) v = 1;
+            return v;
         })
         .attr("fill", function(d,i){
             return colorScales[i](d);
         });
     }
-    var rangeY = 40;
-    var scalesY = [];
-    domains.forEach(function(d){
-        scalesY.push( d3.scaleLinear()
-        .domain(d)
-        .range([0,rangeY]));
-    });
-    var colorScales = [];
-    domains.forEach(function(d){
-        colorScales.push( d3.scaleLinear()
-        .domain(d)
-        .range(["blue","red"]));
-    });
-    
 
-    var svg = d3.select("#drawarea").attr("width", 800).attr("height", 620);
-    var text_date;
-
-    var projection;
     d3.json("./data/hokkaido_topo.json",function(geodata_topo){
 
         var hokkaido_geo = topojson.merge(geodata_topo,geodata_topo.objects.hokkaido.geometries.filter(function(d){return d.properties.N03_001 == "北海道"}));
@@ -176,7 +161,30 @@ function init(){
 
         text_date = d3.select("#drawarea").append("text").attr("x",0).attr("y",600).text("");
 
-        d3.csv("./data/river_height_satsunai.csv").row(row).get(ready);
+
+        d3.csv("./data/siteinfo.csv")
+          .get(function(data){
+            data.forEach(function(d){siteinfos[d.site_id]=d})
+            console.log(siteinfos);
+            d3.csv("./data/waterlevel_201608.csv").row(row).get(ready);
+          })
+          .row(function(d){
+                function parseLonLat(x){
+                    var reg = new RegExp("[度分秒]");
+                    var row = x.split(reg);
+                    return +row[0] + (+row[1])/60 + (+row[2])/60/60;
+                }
+                Object.keys(d).forEach(function(k){
+                    if(k=="coordinate") {
+                        var lon,lat;
+                        var row = d[k].split(" ");
+                        lat = parseLonLat(row[1]);
+                        lon = parseLonLat(row[3]);
+                        d[k] = [lon,lat];
+                    }
+                });
+                return d;
+          });
     });
 
 }
